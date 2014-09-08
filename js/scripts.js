f37a81943c2c82807502803f71dd85342713a1ee
agonzadu <agonzadu@gmail.com>
2014-09-08 19:55:17 +0200

    Primer ejercicio de JavaScript
    Definiciones de variables y prácticas.

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+// Prueba de funcionamiento de JS
+//document.write("Hola");
+
+// Generar alerta en a usuario
+// alert("Ventana de alerta para usaurio");
+
+// Solicitar información a nuestro usuario
+/* 
+var nombre = prompt("Por favor introduzca su nombre");
+document.write("<h1>Bienvenid@: " + nombre + "</h1><br />");
+
+var edad = prompt("Por favor introduzaca su Edad");
+document.write("Su edad es: " + edad + " años"); */
+
+// Operaciones matemáticas
+
+/*var numUno = 5;
+var numDos = 2;
+var numTres = 2;
+
+var suma = numUno + numDos;
+
+var resultado = suma / numTres;
+
+console.log(suma);
+
+console.log(resultado);*/
+
+// Operaciones matemáticas personalizadas
+
+/*var numUno = parseInt(prompt("Introduzca Primer valor"));
+var numDos = parseInt(prompt("Introduzca Segundo valor"));
+var numTres = parseInt(prompt("Introduzca Tercer valor"));
+
+var suma = numUno + numDos;
+
+var resultado = suma / numTres;
+
+console.log("La operación es la siguiente: " + "(" + numUno + " + " + numDos + ")" + "/" + numTres);
+
+console.log(suma);
+
+console.log(resultado);*/
