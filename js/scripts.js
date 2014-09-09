c3715abb4c577e3301c05ea834c67680be5712f6
agonzadu <agonzadu@gmail.com>
2014-09-09 21:16:15 +0200

    Practica individual
    Practica individual

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,62 @@ console.log("La operación es la siguiente: " + "(" + numUno + " + " + numDos + 
 console.log(suma);
 
 console.log(resultado);*/
+
+//Variables Booleanas
+
+/*var numUno = parseInt(prompt("Introduzca un Número"));
+//var numDos = parseInt(prompt("Introduzca un Número"));
+
+var resultado = numUno >= 18;
+console.log(resultado);
+*/
+
+//Variables Arrays - Cadenas
+
+/*var meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+
+var semana = [1,2,3,4,5,6,7,8,9];
+
+console.log(meses);
+
+console.log(meses[3]);
+
+console.log(semana[5]);
+*/
+
+// ******************* Practica individual matemáticas *************************************
+
+/*var numUno = parseInt(prompt("Introduzca valor 1:"));
+var numDos = parseInt(prompt("Introduzca valor 2:"));
+var numTres = parseInt(prompt("Introduzca valor 3:"));
+
+var resultadoSuma = numUno + numDos + numTres;
+var resultadoResta = numUno - numDos - numTres;
+var producto = numUno * numDos * numTres;
+var promedio = resultadoSuma / 3;
+
+
+document.write("<h3>Resultado de la Suma: " + resultadoSuma + "</h3>");
+document.write("<h3>Resultado de la Resta: " + resultadoResta + "</h3>");
+document.write("<h3>Resultado del producto: " + producto + "</h3>");
+document.write("<h3>Promedio: " + promedio + "</h3>");
+
+var resultados = alert("Suma: " + resultadoSuma + "\n" + "Resta: " + resultadoResta + "\n" + "Producto: " + producto + "\n" + "Promedio: " + promedio);
+*/
+
+var acceso = parseInt(prompt("Introduzca su edad:"));
+
+if (acceso >= 18){
+	alert("Bienvenido a la página")
+	document.write("<h1>Bienvenido, por favor, siga</h1>")
+}
+
+else if(acceso = 80){
+	alert("Abueloooo!!!!!")
+}
+
+else{
+	alert("Por favor, abandone la página")
+	document.write("<h1>FUERA DE AQUI!!!!</h1>")
+}
+
